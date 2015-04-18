590249b97e978dfc880cd291ae0a8c76013d0ce3
Nathaniel Foster <nathanielsmusic@gmail.com>
2015-04-18 12:34:40 -0400

    moved comments

--- js/libs/DeviceOrientationControls.js
+++ js/libs/DeviceOrientationControls.js
@@ -43,13 +43,17 @@ THREE.DeviceOrientationControls = function ( object ) {
 
 		return function ( quaternion, alpha, beta, gamma, orient ) {
 
-			euler.set( beta, alpha, - gamma, 'YXZ' );                       // 'ZXY' for the device, but 'YXZ' for us
+			// 'ZXY' for the device, but 'YXZ' for us
+			euler.set( beta, alpha, - gamma, 'YXZ' );
 
-			quaternion.setFromEuler( euler );                               // orient the device
+			// orient the device
+			quaternion.setFromEuler( euler );
 
-			quaternion.multiply( q1 );                                      // camera looks out the back of the device, not the top
+			// camera looks out the back of the device, not the top
+			quaternion.multiply( q1 );
 
-			quaternion.multiply( q0.setFromAxisAngle( zee, - orient ) );    // adjust for screen orientation
+			// adjust for screen orientation
+			quaternion.multiply( q0.setFromAxisAngle( zee, - orient ) );
 
 		}
 
